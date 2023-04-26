6e882f061a6d73548660df2e3d16b85b24451521
Jay <jaykapadia389@gmail.com>
2023-04-26 23:32:56 +0530

    started sudoku solver

--- routes/api.js
+++ routes/api.js
@@ -9,10 +9,35 @@ module.exports = function (app) {
   app.route('/api/check')
     .post((req, res) => {
 
+      let {
+        puzzle,
+        coordinate,
+        value
+      } = res.body ; 
+
     });
     
   app.route('/api/solve')
     .post((req, res) => {
 
+    let regex = /^1-9\./;  
+    let puzzle = req.body.puzzle;
+
+    if(!puzzle){
+      res.json({ error: 'Required field missing' });
+    }  
+
+    if(regex.test(puzzle)){
+      res.json({ error: 'Invalid characters in puzzle' });
+    }  
+      
+    if(puzzle.length !== 81){
+      res.json({ error: 'Expected puzzle to be 81 characters long' });
+    }  
+
+      let solution = SudokuSolver.solve(puzzle);
+    if (!solution) return res.json({ "error": "Puzzle cannot be solved" });
+
+     res.json({ solution : solution}) ;
     });
 };
